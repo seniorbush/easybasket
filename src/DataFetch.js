2c9b19bb896a7062bca00924f4bcc9c75ea7dc89
Philip Moodie <philipmoodie1989@hotmail.co.uk>
2023-01-21 19:39:17 +0000

    initial commit

---
+++ src/DataFetch.js
@@ -0,0 +1,34 @@
+import { useEffect, useState} from 'react'
+import axios from 'axios'
+
+function DataFetch() {
+    const [recipes, setRecipes] = useState([])
+
+
+    useEffect(() => {
+        axios
+        .get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${"spaghetti"}cuisine="italian"&excludeIngredients="peanuts"&instructionsRequired&number=3`)
+        .then(res => {
+            setRecipes(res.data.results)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+    
+
+    return (
+        <div>
+            {/* <input type="text"/> */}
+            <ul>
+                {
+                recipes.map
+                ((recipe) => (<li key={recipe.id}>{recipe.title}</li>))
+                }
+            </ul>
+        </div>
+        
+    )
+}
+
+export default DataFetch
