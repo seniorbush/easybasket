a6e862170e8581bdb268f6cd33d2873bd4a08e28
Philip Moodie <philipmoodie1989@hotmail.co.uk>
2023-02-06 22:22:59 +0000

    updated shopping list to append multiple recipes

--- src/Hooks/useRecipe.js
+++ src/Hooks/useRecipe.js
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
-import axios
- from "axios";
+import axios from "axios";
  
 export const useRecipe = (id) => {
-    const [ingredients, setIngredients] = useState(null)
+    const [ingredients, setIngredients] = useState([])
 
     useEffect(() => {
         axios.get(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${process.env.REACT_APP_API_KEY}`)
-        .then(res => {setIngredients(res.data.extendedIngredients)})
+        .then(res => {setIngredients(ingredients => [...ingredients, ...res.data.extendedIngredients])})
         .catch(err => {console.log(err)})
-    }, [id])
+    }, [id] )
 
     
     
